64117fade7ef571d1f1825b8821cc5162c407357
Kristanto Sean <kristantosean@gmail.com>
2025-06-17 10:22:37 +0700

    Initial Commit

---
+++ FaceDetection/FaceDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceDetectionApp.swift
+//  FaceDetection
+//
+//  Created by Kristanto Sean on 2025-06-17.
+//
+
+import SwiftUI
+
+@main
+struct FaceDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
